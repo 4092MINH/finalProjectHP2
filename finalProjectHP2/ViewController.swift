89a5973d97aa7d744336e071953c1bdd135dd16b
Phan Nguyen Khanh Minh <phannguyenkhanhminh@gmail.com>
2024-08-11 10:06:27 +0700

    Initial Commit

---
+++ finalProjectHP2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  finalProjectHP2
+//
+//  Created by Phan Nguyễn Khánh Minh on 11/8/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
